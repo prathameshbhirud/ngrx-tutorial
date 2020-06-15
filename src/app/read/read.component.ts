f6d747a67418b9f883b60fdf8a2e9b7fe2108782
Prathamesh <pratham_21289@yahoo.co.in>
2020-06-15 20:27:43 +0530

    files added

---
+++ src/app/read/read.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { Tutorial } from './../models/tutorial.model';
+import { AppState } from './../app.state';
+
+@Component({
+  selector: 'app-read',
+  templateUrl: './read.component.html',
+  styleUrls: ['./read.component.css']
+})
+export class ReadComponent implements OnInit {
+
+  tutorials: Observable<Tutorial[]>;
+
+  constructor(private store: Store<AppState>) {
+    this.tutorials = store.select('tutorial');
+  }
+
+  ngOnInit() {
+  }
+
+}
